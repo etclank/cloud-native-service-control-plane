6ccec30721aeaa587403a7554b92f018a51eefa9
Eoghan Clancy <103181608+etclank@users.noreply.github.com>
2026-07-17 10:15:08 +0200

    Add immutable ManagedService demo image

---
+++ cmd/demo-http/main.go
@@ -0,0 +1,99 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/etclank/cloud-native-service-control-plane/internal/demohttp"
+)
+
+const shutdownTimeout = 10 * time.Second
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error("Demo HTTP server failed", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	port, err := demohttp.ParsePort(os.Getenv("PORT"))
+	if err != nil {
+		return fmt.Errorf("configure server: %w", err)
+	}
+
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%d", port),
+		Handler:           demohttp.NewHandler(os.Getenv("MESSAGE")),
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	serverErrors := make(chan error, 1)
+	slog.Info("Starting demo HTTP server", "address", server.Addr)
+	go func() {
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	select {
+	case err := <-serverErrors:
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("serve HTTP: %w", err)
+		}
+
+		return nil
+	case <-ctx.Done():
+		slog.Info("Shutting down demo HTTP server")
+
+		shutdownCtx, cancel := context.WithTimeout(
+			context.Background(),
+			shutdownTimeout,
+		)
+		defer cancel()
+
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			return fmt.Errorf("shut down HTTP server: %w", err)
+		}
+
+		if err := <-serverErrors; err != nil &&
+			!errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("serve HTTP: %w", err)
+		}
+
+		slog.Info("Demo HTTP server stopped")
+
+		return nil
+	}
+}
